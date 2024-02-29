479b070c9a31e92df4609c8cbb118f590f29d5cc
Daniel-Botache <daniel.botache@gmail.com>
2024-02-29 17:35:47 -0500

    cambios en el cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -23,10 +23,14 @@ app.use(cors({
 app.use("/", router);
 
 // Middleware para manejar el CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://desarrollador-junior-iprocess-v2j8.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+server.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
