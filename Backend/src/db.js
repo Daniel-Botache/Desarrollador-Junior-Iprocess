871dd62b6602f5c09262f1ee13168366dd1827d6
Daniel-Botache <daniel.botache@gmail.com>
2024-02-29 17:49:44 -0500

    se hace redeploy del back

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -37,7 +37,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { railway } = sequelize.models;
+const { User } = sequelize.models;
 
 module.exports = {
     ...sequelize.models,
