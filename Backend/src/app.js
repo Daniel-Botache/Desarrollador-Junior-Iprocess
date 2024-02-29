a117c23fe79ac5b949fe19510300a879d5e52245
Daniel-Botache <daniel.botache@gmail.com>
2024-02-29 17:39:41 -0500

    se hacen cambios en cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -23,7 +23,7 @@ app.use(cors({
 app.use("/", router);
 
 // Middleware para manejar el CORS
-server.use((req, res, next) => {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
