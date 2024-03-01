7934fdb38765cd17b3e0eb3956b9c6400ccc91f9
Daniel-Botache <daniel.botache@gmail.com>
2024-02-29 23:52:36 -0500

    se hace cambio en el back para railway

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -6,11 +6,12 @@ const path = require("path");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 const sequelize = new Sequelize(
-    `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-  {
-    logging: false,
-    native: false,
-  }
+  `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  { logging: false, native: false, dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Deshabilitar la verificación del certificado para fines de prueba (no recomendado en producción)
+    }, }}
 );
 const basename = path.basename(__filename);
 
